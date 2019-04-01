bb540aa6dfcfa29b0782761078694c20f9f524a9
abanty <abantybredda@gmail.com>
2019-04-01 11:50:06 -0500

    cambio recientes

--- js/portfolio/setting.js
+++ js/portfolio/setting.js
@@ -5,15 +5,15 @@ if (jQuery().quicksand) {
 
  	// Clone applications to get a second collection
 	var $data = $(".portfolio").clone();
-	
+
 	//NOTE: Only filter on the main portfolio page, not on the subcategory pages
 	$('.filter li').click(function(e) {
-		$(".filter li").removeClass("active");	
+		$(".filter li").removeClass("active");
 		// Use the last category class as the category to filter by. This means that multiple categories are not supported (yet)
 		var filterClass=$(this).attr('class').split(' ').slice(-1)[0];
-		
-		if (filterClass == 'all') {
-			var $filteredData = $data.find('.item-thumbs');
+
+		if (filterClass == 'construccion') {
+			var $filteredData = $data.find('.construccion');
 		} else {
 			var $filteredData = $data.find('.item-thumbs[data-type=' + filterClass + ']');
 		}
@@ -21,9 +21,9 @@ if (jQuery().quicksand) {
 			duration: 600,
 			adjustHeight: 'auto'
 		}, function () {
-		
+
 			// Portfolio fancybox
-		$(".fancybox").fancybox({				
+		$(".fancybox").fancybox({
 				padding : 0,
 				beforeShow: function () {
 					this.title = $(this.element).attr('title');
@@ -35,11 +35,11 @@ if (jQuery().quicksand) {
 			});
 
 
-		});	
-		$(this).addClass("active"); 			
+		});
+		$(this).addClass("active");
 		return false;
 	});
-	
+
 }//if quicksand
 
 });
